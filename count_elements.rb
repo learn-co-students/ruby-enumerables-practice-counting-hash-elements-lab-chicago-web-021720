b785d2ebaa1836c450379cafc95e0bf49ab2971b
Sam Escobar <sescobar8714@gmail.com>
2020-03-10 13:58:24 -0700

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
+
+  # count how many values are in 
+  #each key 
+
+  new_hash = Hash.new(0)# counter hashes 
+
+  array.each do |item|
+    new_hash[item] += 1
+  end
+  new_hash
   # code goes here
 end
  
